dcd958480f1b72bec1525e562916a8b11a09ef9e
Vikas0894 <vikas.k@dntinfotech.com>
2024-03-28 12:52:35 +0530

    jenkins issue

--- src/helper/browser/browser-get-url.ts
+++ src/helper/browser/browser-get-url.ts
@@ -1,5 +1,3 @@
-import { browser } from '@wdio/globals'
-
 export const browserGetUrl = async (): Promise<string> => {
     return await browser.getUrl();
 };
--- src/helper/browser/browser-get-window-handles.ts
+++ src/helper/browser/browser-get-window-handles.ts
@@ -1,5 +1,3 @@
-import { browser } from '@wdio/globals'
-
 export const browserGetWindowHandles = async (): Promise<Array<string>> => {
     return browser.getWindowHandles();
 };
